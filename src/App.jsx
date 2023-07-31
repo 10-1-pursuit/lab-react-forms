bfb1be19666cbc23e903fda156bf190224c9e5b5
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-07-30 21:04:22 -0400

    added data to Form.jsx- the calcualtion is working on for for sum and average

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Form from "./Form";
 import "./App.css";
 
+
+
 function App() {
   return (
     <main>
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,63 @@
 import React from "react";
 import "./Form.css";
+import {useState} from "react";
 
 function Form() {
+const [input, setUserInput] =useState("")
+const [operation, setOperation] =useState("")
+const [result, setResult] = useState("")
+
+function  handleUserInput(event){
+  setUserInput(event.target.value);
+
+}
+function handleOperation(event){
+  setOperation(event.target.value);
+
+}
+const inputArr =input.split(",");
+const numArr = inputArr.map((num)=> Number(num));
+
+const calculateNumArr = (operation)=>{
+  switch (operation){
+    case "sum":
+      getSum(numArr);
+      break;
+      case "average":
+        getAverage(numArr);
+        break;
+        case "mode":
+          getMode(numArr);
+          break;
+          default:
+            setResult("Invalid input.");
+
+  }
+  function getSum(numArr) {
+    const sum =numArr.reduce((total, num)=>{
+      return total + num
+    },0);
+    setResult(sum)
+  }
+  function getAverage(numArr){
+    const numSum =numArr.reduce((total, num)=>{
+      return total + Number(num)
+    },0)
+    const numAverage = numSum / numArr.length + 1
+setResult(numAverage)
+  }
+}
+function handleSubmit(event){
+  event.preventDefault();
+  calculateNumArr(operation);
+}
+
   return (
+    <main>
     <>
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={handleSubmit}>
+        <input id="values" name="values" type="text" value={input} onChange={handleUserInput}/>
+        <select id="operation" name="operation" onChange={handleOperation}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
@@ -15,9 +66,11 @@ function Form() {
         <button type="submit">Calculate</button>
       </form>
       <section id="result">
-        <p></p>
+        <p>{result}</p>
       </section>
+    
     </>
+    </main>
   );
 }
 
