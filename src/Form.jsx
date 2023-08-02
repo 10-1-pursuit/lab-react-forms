4ac4e610db11d86c79811886afe99cd451c5f4ef
xavierrice <xavier.rice@gmail.com>
2023-08-01 20:09:43 -0400

    Nice-got my form workin, so how do i retrive the option and the numbers?

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { useState } from "react";
 import "./Form.css";
 
 function Form() {
+  
+  const [valueArray, setValueArray] = useState([])
+  
+  function handleValueArray(event){
+    setValueArray(event.target.value)
+    console.log("this is the synthEvent:", event.target.value)
+  }
+  
+  function handleSubmit(event){
+   event.preventDefault()
+   console.log("form Submitted", event.target.array.value)
+  
+  //THIS IS WHERE I WOULD DO LOGIC??//
+  
+  }
+  
+  
   return (
     <>
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={handleSubmit}>
+        <input id="array" name="values" type="text" />
+        <select id="operation" name="operation" onChange={handleValueArray}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
