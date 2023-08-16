6d3057ff01b24b3b9b8730078278e0a0199a53a7
Smille007 <smille007@gmai.com>
2023-08-16 10:12:43 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,55 @@
-import React from "react";
-import Form from "./Form";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
+import Form from './Form'; // Import the Form component
 
 function App() {
+  const [result, setResult] = useState('');
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+
+    const valuesInput = document.getElementById('values');
+    const operationSelect = document.getElementById('operation');
+    const resultParagraph = document.querySelector('#result p');
+
+    const inputNumbers = valuesInput.value;
+    const operation = operationSelect.value;
+
+    const numbersArray = inputNumbers.split(',').map(number => Number(number.trim()));
+
+    if (numbersArray.some(isNaN)) {
+      resultParagraph.textContent = 'Invalid input.';
+      return;
+    }
+
+    let calculatedResult;
+
+    if (operation === 'sum') {
+      calculatedResult = numbersArray.reduce((acc, val) => acc + val, 0);
+    } else if (operation === 'average') {
+      calculatedResult = numbersArray.reduce((acc, val) => acc + val, 0) / numbersArray.length;
+    } else if (operation === 'mode') {
+      const occurrences = {};
+      numbersArray.forEach(num => {
+        occurrences[num] = (occurrences[num] || 0) + 1;
+      });
+      const mode = Object.keys(occurrences).reduce((a, b) =>
+        occurrences[a] > occurrences[b] ? a : b
+      );
+      calculatedResult = mode;
+    }
+
+    resultParagraph.textContent = calculatedResult;
+  };
+
   return (
-    <main>
-      <p>Enter each number in the array, separated by a ','</p>
+    <div className="App">
+      <h1>Number Calculator</h1>
       <Form />
-    </main>
+      
+
+      </div>
+ 
   );
 }
 
